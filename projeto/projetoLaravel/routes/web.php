47df049152655544a242e246c9ff24c384fd5f1b
Natanael <juniornatan986@gmail.com>
2022-08-24 03:07:46 -0300

    comit

--- projeto/projetoLaravel/routes/web.php
+++ projeto/projetoLaravel/routes/web.php
@@ -13,6 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::prefix('usuarios')->group(function(){
+    Route::get('',function(){
+        return 'usuario';
+    });
+
+    Route::get('/{id}', function($id){
+        return 'Mostrar detalhes usuario'.$id;
+    });
+
+    Route::get('/{id}/tags', function($id){
+        return 'Tags do usuario'.$id;
+    });
+    
+});
+
+Route::get('/contato/{paramA}/{paramB}', function($paramA, $paramB){
+    return 'Parametros: '. $paramA. '-'. $paramB;
+});
+
+Route::get('/users/{id}', function($id){
+    return 'Id Usuario: '.$id;
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
